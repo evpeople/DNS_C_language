f8a53c8ac3d74dc6d80b265fa6fe907b45c3f93c
wangzhe <hangcaihui@gmail.com>
2021-04-25 01:01:25 +0800

    更好的版本

--- lab1/datalink.c
+++ lab1/datalink.c
@@ -7,7 +7,7 @@
 
 #define DATA_TIMER 2000
 #define BUFFERS_NUM 10
-static unsigned char send_buffers[BUFFERS_NUM][PKT_LEN];
+static unsigned char send_buffers[BUFFERS_NUM + 1][PKT_LEN];
 static unsigned char frame_expected = 0;
 static unsigned int number_of_send = 0;
 static unsigned char get_buffer[PKT_LEN];
@@ -16,7 +16,11 @@ static unsigned char have_send[BUFFERS_NUM + 1];
 static unsigned char have_arrived[BUFFERS_NUM + 1];
 
 static bool phl_ready = false;
-
+#define inc(k)           \
+    if (k < BUFFERS_NUM) \
+        k = k + 1;       \
+    else                 \
+        k = 0
 struct FRAME
 {
     unsigned char kind; /* FRAME_DATA */
@@ -58,12 +62,12 @@ int main(int argc, char **argv)
         {
         case NETWORK_LAYER_READY: //可以发送新的数据了（可以从网络层拿包了
             dbg_warning("进入了网络层准备好、\n");
-            get_packet(send_buffers[number_of_send % BUFFERS_NUM]);
-            have_send[number_of_send % BUFFERS_NUM] = 1;
-            have_ack[number_of_send % BUFFERS_NUM] = 0;
-            send_data_frame(number_of_send % BUFFERS_NUM);
-            number_of_send++;
-
+            get_packet(send_buffers[number_of_send]);
+            have_send[number_of_send] = 1;
+            have_ack[number_of_send] = 0;
+            send_data_frame(number_of_send);
+            // number_of_send++;
+            inc(number_of_send);
             dbg_error("完成网络层准备好后的 numOFSEND %d\n", number_of_send);
             break;
         case PHYSICAL_LAYER_READY:
@@ -104,25 +108,25 @@ int main(int argc, char **argv)
                     send_ack_frame(to_network.seq);
                     have_arrived[to_network.seq] = 1;
                 }
-                else if (have_arrived[to_network.seq])
-                {
-                    /* code */
-                    send_ack_frame(to_network.seq);
-                }
+                // else if (have_arrived[to_network.seq])
+                // {
+                //     /* code */
+                //     send_ack_frame(to_network.seq);
+                // }
             }
             if (to_network.kind == FRAME_ACK)
             {
                 dbg_error("***************************************收到ACK后的 numOFSEND*************%d\n", number_of_send);
-                dbg_event("收到了  ACK  帧， ACK  号是 %d not_receive 是 %d  number_OF_SEND  %d \n", to_network.ack, not_recvive, number_of_send % BUFFERS_NUM);
-                if (in_middle(to_network.ack, not_recvive, number_of_send % BUFFERS_NUM)) //大于就算成功，能大于说明之前也得到了
+                dbg_event("收到了  ACK  帧， ACK  号是 %d not_receive 是 %d  number_OF_SEND  %d \n", to_network.ack, not_recvive, number_of_send);
+                if (in_middle(to_network.ack, not_recvive, number_of_send)) //大于就算成功，能大于说明之前也得到了
                 {
                     dbg_frame("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~测试in_middle 成功\n");
                     dbg_error("#######################到have_ack[not_recvive] = 1;后的 numOFSEND*************%d\n", number_of_send);
                     have_ack[not_recvive] = 1;
-                    for (size_t i = (not_recvive + 1) % BUFFERS_NUM; i <= to_network.ack; i++)
-                    {
-                        have_ack[i] = 1;
-                    }
+                    // for (size_t i = (not_recvive + 1) % BUFFERS_NUM; i <= to_network.ack; i++)
+                    // {
+                    //     have_ack[i] = 1;
+                    // }
                     not_recvive = to_network.ack;
                     dbg_error("#######################到 have_ack[1 + BUFFERS_NUM]++;后的 numOFSEND*************%d\n", number_of_send);
                     have_ack[1 + BUFFERS_NUM]++;
@@ -150,6 +154,7 @@ int main(int argc, char **argv)
             {
                 if (number_of_send + 1 >= BUFFERS_NUM)
                 {
+                    dbg_frame("被重发的都有%d 判断一下是否有过ACK%d \n", i, have_ack[i]);
                     if (!have_ack[i])
                     {
                         // dbg_frame("have_ack = %d\n", have_ack[i]);
@@ -225,14 +230,14 @@ static void send_nak_frame(unsigned char to_send_nak)
 }
 static void chang_number(unsigned char *t)
 {
-    if (*t >= BUFFERS_NUM)
+    if (*t > BUFFERS_NUM)
     {
         *t = 0;
     }
 }
 static bool in_middle(unsigned char a, unsigned char b, unsigned char c)
 {
-    if (((a <= b) && (b <= c)) || ((b <= c) && (c <= a)) || ((c <= a) && (a <= b)))
+    if (((a <= b) && (b < c)) || ((b < c) && (c <= a)) || ((c < a) && (a <= b)))
     {
         return true;
     }
